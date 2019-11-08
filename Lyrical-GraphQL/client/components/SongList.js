a4f23b053f40f406e08f6e7362ec6794f5504ed6
Azlan Cuttilan <jainudeen.cuttilan@justgiving.com>
2019-11-08 15:09:02 +0000

    41. Fixing key warnings

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -5,7 +5,11 @@ import { graphql } from 'react-apollo';
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => {
-      return <li>{song.title}</li>;
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
     });
   }
 
@@ -13,13 +17,14 @@ class SongList extends Component {
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
-    return <div>{this.renderSongs()}</div>;
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
