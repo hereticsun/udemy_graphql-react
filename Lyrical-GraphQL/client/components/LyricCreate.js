3ffcf81dbb04fb6b9b262a1ac1bd377267bcd207
Azlan Cuttilan <azlan.cuttilan@justgiving.com>
2019-12-06 12:00:52 +0000

    63. Submitting mutations
    Signed-off-by: Azlan Cuttilan <azlan.cuttilan@justgiving.com>

--- Lyrical-GraphQL/client/components/LyricCreate.js
+++ Lyrical-GraphQL/client/components/LyricCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
   constructor(props) {
@@ -9,6 +11,15 @@ class LyricCreate extends Component {
 
   onSubmit(event) {
     event.preventDefault();
+
+    this.props
+      .mutate({
+        variables: {
+          content: this.state.content,
+          songId: this.props.songId
+        }
+      })
+      .then(() => this.setState({ content: '' }));
   }
 
   render() {
@@ -35,4 +46,4 @@ const mutation = gql`
   }
 `;
 
-export default LyricCreate;
+export default graphql(mutation)(LyricCreate);
